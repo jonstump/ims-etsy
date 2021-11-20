f689f2adee688eb0974a4380466413d1cc6485b1
Jon Stump <jmstump@gmail.com>
2021-11-19 20:13:16 -0800

    add test for customer and inventory models

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe customer, type: :model do
-  it {  }
+describe Customer do
+  it { should validate_presence_of :first_name }
+  it { should validate_presence_of :last_name }
+  it { should validate_presence_of :email }
 end
--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
-RSpec.describe Inventory, type: :model do
-  it {  }
+describe Inventory do
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :price }
+  it { should validate_presence_of :sku }
 end
