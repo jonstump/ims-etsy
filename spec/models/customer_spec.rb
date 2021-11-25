ced3c95fbb61a9b2fde66e125f785732c6ce3e7f
Jon Stump <jmstump@gmail.com>
2021-11-24 23:09:04 -0800

    update class name to match other model related files

--- spec/models/customer_spec.rb
+++ spec/models/customer_spec.rb
@@ -1,6 +1,6 @@
 require 'rails_helper'
 
-describe Customer do
+describe Customers do
   it { should validate_presence_of :first_name }
   it { should validate_presence_of :last_name }
   it { should validate_presence_of :email }
