d0cde442dcada367a98dbd1aeec692e062eb2e54
Jon Stump <jmstump@gmail.com>
2021-11-27 11:27:00 -0800

    update customer app model

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,6 @@
-class Customer < ApplicationRecord
-  validates *%i[first_name last_name email],
-    presence: true
+# Customers class is meant to check names and emails
+class Customers < ApplicationRecord
+  validates :first_name, presence: true
+  validates :second_name, presence: true
+  validates :email, presence: true
 end
